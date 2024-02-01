aef5c12caadf3ce787c065616b84836ce95a878d
matteo di cicco <39630695+matteodicicco@users.noreply.github.com>
2024-02-01 23:00:52 +0000

    tet

--- codice.c
+++ codice.c
@@ -2,7 +2,7 @@
 
 void fase1()  
 {
-    int quant_art, dove_sped;
+    int quant_art, dove_sped, var;
     float costo_art, costoart_tot, tot_articoli, iva, sped_it, sped_ww, tot_it, tot_ww;
     iva = costo_art * 0.22;
     sped_it = 10;
@@ -11,23 +11,26 @@ void fase1()
     tot_it = quant_art + iva + sped_it;
     tot_ww = quant_art + iva + sped_ww;
 
-    printf("Dove desideri spedire il tuo ordine: \n1) Italia \n2) Europa \n3) Resto del Mondo");
-    scanf("%d", &dove_sped);
+    while (var == 0)
+    {
+        printf("Dove desideri spedire il tuo ordine: \n1) Italia \n2) Europa \n3) Resto del Mondo");
+        scanf("%d", &dove_sped);
         if (dove_sped == 1)
         {
-            printf("Il totale del tuo ordine è:\n€ %f \n€ %f (IVA 22\%)€ %f (Spedizione 24/48 ore) \n");
+            printf("\n\nIl totale del tuo ordine è:\n\n€ %f \n\n€ %f (IVA 22\%) \n\n€ %f (Spedizione 24/48 ore) \n/nPer un totale di € %f \n\n", costoart_tot, iva, sped_it, tot_it);
+            var = 1;
         }
         else if (dove_sped == 2)
         {
-            /* code */
+            printf("\n\nIl totale del tuo ordine è:\n\n€ %f \n\n€ %f (IVA 22\%) \n\n€ %f (Spedizione 24/48 ore) \n/nPer un totale di € %f \n\n", costoart_tot, iva, sped_ww, tot_ww);
+            var = 1;
         }
         else if (dove_sped == 3)
         {
-            /* code */
+            printf("\n\nIl totale del tuo ordine è:\n\n€ %f \n\n€ %f (IVA 22\%) \n\n€ %f (Spedizione 24/48 ore) \n/nPer un totale di € %f \n\n", costoart_tot, iva, sped_ww, tot_ww);
+            var = 1;
         }
-        else printf("\nInserire un numero tra 1, 2 e 3.\n");
-        
-    printf("\n\n");
-    printf("\nIl totale del tuo carrello è:\n€ %f\n");
+        else printf("\n\nInserire un numero tra 1, 2 e 3.\n\n");
+    }
 
 }
--- main.c
+++ main.c
@@ -12,19 +12,19 @@ int main()
     int pulizia;
     int lavanderia;
     int cucinaart;
+    int nulla;
 
-    printf("Benvenuti su EasyRental, qui troverai a tua disposizione tutto ciò di cui hai bisogno solo una volta .. perchè comprare un articolo quando puoi noleggiarne uno?\n");
-    printf("cosa vorresti noleggiare?\n");
-    printf("seleziona la categoria:\n 1)?\n 2)elettrodomestici\n 3)articoli per la casa\n 4)strumenti ed articoli musicali\n 5)giardinaggio e articoli da giardino\n ");
+    printf("\n\nBENVENUTI SU EASYRENTAL! \n\nQui potrete trovare a vostra disposizione moltissimi articoli cui avete bisogno, o di cui avrete avuto bisogno, anche solo una volta nella vostra vita. \n\nPerchè comprare un articolo quando puoi noleggiarlo? \n\nSfoglia pure il nostro catalogo completo per verificare la presenza delll'articolo da te cercato! \n\n\n\n");
+    printf("Seleziona la categoria interessata:\n\n 1) Elettrodomestici\n 2) Articoli per la casa\n 3) Strumenti ed articoli musicali\n 4) Giardinaggio e articoli da giardino\n\n\n ");
     scanf("%d", &articoli);
 
-    if (articoli == 2)
+    if (articoli == 1)
     {
-        printf("articoli elettrodomestici;\n scegli la tua categoria:\n 1)cucina\n 2)pulizia\n 3)lavanderia\n");
+        printf("\n\n\nStai cercando elettrodomestici relativi a:\n 1) Cucina\n 2) Pulizia\n 3) Lavanderia\n");
         scanf("%d", &elettrodomestici);
         if(elettrodomestici == 1)
         {
-            printf("scegli tra:\n 1)impastatrice\n 2)friggitrice\n 3)affettatrice\n 4)macchina per popocorn\n 5)gelatiera\n 6)macchina per lo zucchero filato\n 7)affumicatore\n 8)macchina sottovuoto\n 9)macchina per la pasta\n 10)essiccatore\n 11)yogurtiera\n");
+            printf("A disposizione tra gli elettrodomestici per la cucina abbiamo:\n 1)impastatrice\n 2)friggitrice\n 3)affettatrice\n 4)macchina per popocorn\n 5)gelatiera\n 6)macchina per lo zucchero filato\n 7)affumicatore\n 8)macchina sottovuoto\n 9)macchina per la pasta\n 10)essiccatore\n 11)yogurtiera\n");
             scanf("%d", &cucina );
         }
         else if(elettrodomestici == 2)
@@ -32,13 +32,13 @@ int main()
             printf("scegli tra:\n 1)robot aspirapolvere \n 2)idropulitrice\n 3)lavapavimenti\n 4)spazzacamino\n 5)lucidatrice\n 6)aspiraliquidi\n 7)lavamoquette\n");
             scanf("%d", &pulizia);
         }
-        else if
+        else if(elettrodomestici == 3)
         {
             printf("scegli tra: 1)macchina da cucire\n 2)lavatrice\n 3)asciugatrice\n 4)stira asciuga pantaloni\n");
             scanf("%d", &lavanderia);
         }
     }
-    else if (articoli == 3)
+    else if (articoli == 2)
     {
         printf("articoli per la casa:\n scegli la tua categoria:\n 1)cucina\n 2)camera da letto\n 3)bagno\n 4)salone\n 5)lavanderia\n");
         scanf("%d", &casa);
@@ -48,12 +48,12 @@ int main()
             scanf("%d",&cucinaart );
         }
     }
-    else if (articoli == 4)
+    else if (articoli == 3)
     {
         printf("strumenti ed articoli musicali:\n seleziona l'articolo:\n 1)chitarra\n 2)batteria\n 3)flauto\n 4)pianoforte\n 5)pianola\n 6)basso\n 7)sassofono\n 8)fisarmonica\n 9)violino\n 10)violoncello\n 11)giradischi\n 12)stereo\n");
         scanf("%d", &musica);
     }
-    else if (articoli == 5)
+    else if (articoli == 4)
     {
         printf("giardinaggio e articoli da giardino:\n seleziona i tuoi articoli:\n 1)zappa\n 2)");
         scanf("%d", &giardino);
